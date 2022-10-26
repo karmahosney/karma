feb4963afe4dd2b553cc5f5a24417a38a9723b55
karmahosney <116759168+karmahosney@users.noreply.github.com>
2022-10-26 21:29:28 +0200

    Add files via upload

---
+++ LinuxScriptExample.sh
@@ -0,0 +1,38 @@
+ #!/bin/bash
+
+################################################################################
+#                              scriptTemplate                                  #
+#                                                                              #
+# Use this template as the beginning of a new program. Place a short           #
+# description of the script here.                                              #
+#                                                                              #
+# Change History                                                               #
+# 01/01/2020  Hosney Osman    Original code. This is a template for creating   #
+#                           new Bash shell scripts.                            #
+#                           Add new history entries as needed.                 #
+#                                                                              #
+#                                                                              #
+################################################################################
+################################################################################
+############590986##############################################################
+#                                                                              #
+#  Copyright (C) 2007, 2019 Hosney Osman                                       #
+#  eng.hosneyosman@gmail.com                                                   #
+#                                                                              #
+#  This program is free software; you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation; either version 2 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program; if not, write to the Free Software                 #
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                              #
+################################################################################
+################################################################################
+################################################################################
+echo "hello world!"
