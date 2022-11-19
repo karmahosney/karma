62ab9f253c09d02f3e03f0e8d0bd441e02bcdf08
karmahosney <116759168+karmahosney@users.noreply.github.com>
2022-11-19 23:36:15 +0200

    Delete virtualbox_install.bash

--- virtualbox_install.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /etc/yum.repos.d/
-wget http://download.virtualbox.org/virtualbox/rpm/rhel/virtualbox.repo
-dnf -y update
-dnf -y group install "Development Tools"
-dnf -y install kernel-devel
-dnf -y install VirtualBox-7.0.x86_64
